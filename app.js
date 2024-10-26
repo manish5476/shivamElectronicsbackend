21cc1349670aa31d227f0ffce5d14696668ad8e3
Manish Singh <100526847+manish5476@users.noreply.github.com>
2024-10-26 07:25:14 +0000

    error handling

--- app.js
+++ app.js
@@ -32,11 +32,68 @@ app.use("/api/v1/Product", productRoutes);
 
 
 app.all('*',(req,res,next)=>{
-  res.status(404).json({
-    status:'fail',
-    message:`Cant find the Requested url ${req.originalUrl} on Server `
-  })
+  // res.status(404).json({
+  //   status:'fail',
+  //   message:`Cant find the Requested url ${req.originalUrl} on Server `
+  // })
+  const err=new Error(`Cant find the Requested url ${req.originalUrl} on Server `)
+  err.status='fail';
+  err.statusCode=404;
+  next(err);
+})
+
+app.use((err,req,res,next)=>{
+err.statusCode=err.statusCode||500;
+err.status=err.status||"error"
+res.status(err.statusCode).json({
+  status:err.status,
+  message:err.message
+})
+
 })
 
 module.exports = app;
-  
+  // 
+//   const express = require("express");
+// const app = express();
+// app.use(express.json());
+// const morgan = require("morgan");
+
+// const productRoutes = require("./routes/product");
+// // const usersRoutes = require("./routes/auth");
+
+// // if (process.env.NODE_ENV === "development") {
+// //   app.use(morgan("dev"));
+// // }
+// const cors = require("cors");
+// app.use(cors());
+
+// app.use(morgan("combined"));
+
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
+
+// app.use(express.static(`${__dirname}/public/`));
+
+// // app.use("/api/v1/users", usersRoutes);
+// app.use("/api/v1/Product", productRoutes);
+
+// app.all('*', (req, res, next) => {
+//   const err = new Error(`Can't find the requested URL ${req.originalUrl} on this server`);
+//   err.status = 'fail';
+//   err.statusCode = 404;
+//   next(err);
+// });
+
+// app.use((err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || "error";
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message
+//   });
+// });
+
+// module.exports = app;
